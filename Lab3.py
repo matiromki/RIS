43cda3daacb67e08ca45efaa2dd8d719167e088d
Roman Kudryashov <roman76547810@gmail.com>
2024-12-16 23:25:25 +0600

    Upgrade

--- Lab3.py
+++ Lab3.py
@@ -1,37 +1,42 @@
 class Elevator:
     def __init__(self, floor):
         self.floor = floor
+        self.steps = 0
+        self.startOrEnd = 0
+        self.doorState = False
         self.lastCommand = "close_the_door"
-        self.door = False
+
+    def default(self):
         self.end = 0
         self.steps = 0
         
-    def move_up(self):
+    def go_up(self):
         self.floor += 1
         self.steps += 1
-        self.lastCommand = "move_up"
-        return "Move up"
+        self.lastCommand = "go_up"
+        return "Go up"
     
-    def move_down(self):
+    def go_down(self):
         self.floor -= 1
         self.steps += 1
-        self.lastCommand = "move_down"
-        return "Move down"
+        self.lastCommand = "go_down"
+        return "Go down"
 
     def open_the_door(self):
-        self.door = True
-        self.end = 1
+        self.doorState = True
+        self.startOrEnd = 1
         self.lastCommand = "open_the_door"
         return "Open the door"
 
     def close_the_door(self):
-        self.door = False
-        self.lastCommand = "close_the_door"
-        return "Close the door"
+        return self.close()
     
     def close_the_door_and_go(self):
-        self.door = False
-        self.end = 1
+        self.startOrEnd = 1
+        return self.close()
+    
+    def close(self):
+        self.doorState = False
         self.lastCommand = "close_the_door"
         return "Close the door"
 
@@ -39,28 +44,28 @@ class Elevator:
         try:
             return self.Commands[(self.lastCommand, command)](self)
         except KeyError:
-            raise Exception(f"Несуществующая комманда: {command}")
+            raise Exception(f"Команда не существует: {command}")
 
     def open_or_not(self):
-        return self.door
+        return self.doorState
 
     Commands = { 
         ("close_the_door", "open_the_door"): open_the_door,
-        ("move_up", "open_the_door"): open_the_door,
-        ("move_down", "open_the_door"): open_the_door,
+        ("go_up", "open_the_door"): open_the_door,
+        ("go_down", "open_the_door"): open_the_door,
 
         ("open_the_door", "open_the_door"): close_the_door_and_go,
 
-        ("open_the_door", "move_up"): close_the_door,
-        ("close_the_door","move_up"): move_up,
-        ("move_up","move_up"): move_up, 
+        ("open_the_door", "go_up"): close_the_door,
+        ("close_the_door","go_up"): go_up,
+        ("go_up","go_up"): go_up, 
 
-        ("open_the_door","move_down"): close_the_door,
-        ("close_the_door","move_down"): move_down,
-        ("move_down","move_down"): move_down, 
+        ("open_the_door","go_down"): close_the_door,
+        ("close_the_door","go_down"): go_down,
+        ("go_down","go_down"): go_down, 
     }
 
-def CreateSpace(floors = 4, FirstFl = 1, SecondFl = 1):
+def CreateSpace(floors = 4, FloorForFirstEl = 1, FloorForSecondEl = 3):
     first_or_second = [[1] * floors for _ in range(floors)]
 
     actions = {} 
@@ -70,28 +75,28 @@ def CreateSpace(floors = 4, FirstFl = 1, SecondFl = 1):
     for i in range(1, floors+1):
         for j in range(i+1, floors+1):
             first_or_second[i-1][j-1] = 0
-            actions[(i, j)] = "move_up"
-            actions[(j, i)] = "move_down"
+            actions[(i, j)] = "go_up"
+            actions[(j, i)] = "go_down"
 
-    elevators = [Elevator(FirstFl), Elevator(SecondFl)]
+    elevators = [Elevator(FloorForFirstEl), Elevator(FloorForSecondEl)]
     return actions, first_or_second, elevators
 
-calls = ((2, 4), (1, 2), (3, 1), (2, 2), (1, 3))
+calls = (
+    (2, 4), (1, 2), (3, 1), (2, 2), (1, 3), 
+    (4, 1), (3, 2), (1, 4), (2, 1), (3, 3)
+)
 actions, first_or_second, elevators = CreateSpace()
 
 for call in calls:
     n, m = abs(call[0] - elevators[0].floor), abs(call[0] - elevators[1].floor)
-    ElNum = first_or_second[n][m]
+    num = first_or_second[n][m]
+    elevators[num].default()
 
-    elevators[ElNum].end = 0
-    elevators[ElNum].steps = 0
-    print("\n", f"({call[0]} --> {call[1]}) Лифт №{ElNum+1}", end='  ')
+    print(f'Лифт #{num+1} С этажа №{call[0]} на этаж №{call[1]}', end = '\n\t')
 
-    while (elevators[ElNum].floor != call[1]) or (not elevators[ElNum].open_or_not()):
-        print(f"|{elevators[ElNum].floor}|", end = ' ')
+    while (elevators[num].floor != call[1]) or (not elevators[num].open_or_not()):
+        action = actions.get((elevators[num].floor, call[elevators[num].startOrEnd]))
+        result = elevators[num].analyze(action)
+        print(f"|{elevators[num].floor}| {result}", end = ' ')
 
-        action = actions.get((elevators[ElNum].floor, call[elevators[ElNum].end]))
-        result = elevators[ElNum].analyze(action)
-        print(result, end = ' ')
-
-    print(f'\n Этажей пройдено -- {elevators[ElNum].steps} --', end = ' ')
+    print(f'\n\tЭтажей пройдено -- {elevators[num].steps} --')
